27e801c20661e8572d1783ee0f07f4fed263dc6e
BOT22345 <153290500+Nayan2234@users.noreply.github.com>
2025-10-21 14:12:45 +0530

    understanding spring bean basics and dependnecy injection and updated notes

--- Java/SpringBootArchitectureLearning/learningSpringArchitecture/src/main/java/com/practice/learningSpringArchitecture/LearningSpringArchitectureApplication.java
+++ Java/SpringBootArchitectureLearning/learningSpringArchitecture/src/main/java/com/practice/learningSpringArchitecture/LearningSpringArchitectureApplication.java
@@ -1,13 +1,21 @@
 package com.practice.learningSpringArchitecture;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearningSpringArchitectureApplication {
+public class LearningSpringArchitectureApplication implements CommandLineRunner{
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(LearningSpringArchitectureApplication.class, args);
-	}
-
+    }
+    private paymentRazorPay payer;
+    public LearningSpringArchitectureApplication(paymentRazorPay payer){
+        this.payer=payer;
+    }
+    public void run(String... args) throws Exception {
+        String result = payer.pay();
+        System.out.println("Payment attempt result: " + result);
+    }
 }
---
+++ Java/SpringBootArchitectureLearning/learningSpringArchitecture/src/main/java/com/practice/learningSpringArchitecture/paymentRazorPay.java
@@ -0,0 +1,11 @@
+package com.practice.learningSpringArchitecture;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class paymentRazorPay {
+    public String pay(){
+        System.out.println("hello i have payed 2000");
+        return "payed 2000";
+    }
+}
